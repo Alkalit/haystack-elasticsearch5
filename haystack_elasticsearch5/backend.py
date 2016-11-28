0b5e0e12e37c8919b86b14cbdb810e0ea69489fe
Alexey Kalinin <lyoffa412@gmail.com>
2016-11-28 15:48:31 +0300

    initial backend stage

--- haystack_elasticsearch5/backend.py
+++ haystack_elasticsearch5/backend.py
@@ -1,8 +1,17 @@
-from haystack.backends.elasticsearch_backend import ElasticsearchSearchBackend
+from haystack.backends.elasticsearch_backend import ElasticsearchSearchBackend, ElasticsearchSearchQuery
+from haystack.backends import BaseEngine
 
 
-__all__ = ('Elasticsearch5SearchBackend', )
+__all__ = ['Elasticsearch5SearchBackend', 'Elasticsearch5SearchEngine']
 
 
 class Elasticsearch5SearchBackend(ElasticsearchSearchBackend):
     pass
+
+
+class Elasticsearch5SearchQuery(ElasticsearchSearchQuery):
+    pass
+
+class Elasticsearch5SearchEngine(BaseEngine):
+    backend = ElasticsearchSearchBackend
+    query = Elasticsearch5SearchQuery
