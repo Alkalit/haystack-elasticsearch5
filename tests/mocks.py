3665843f8d22616160e0a68a3dd35b7e9e1460a9
Alexey Kalinin <lyoffa412@gmail.com>
2016-11-29 15:21:33 +0300

    fix

--- tests/mocks.py
+++ tests/mocks.py
@@ -7,7 +7,7 @@ from haystack.models import SearchResult
 class MockSearchResult(SearchResult):
     def __init__(self, app_label, model_name, pk, score, **kwargs):
         super(MockSearchResult, self).__init__(app_label, model_name, pk, score, **kwargs)
-        self._model = apps.get_model('core', model_name)
+        self._model = apps.get_model('test_app', model_name)
 
-MOCK_SEARCH_RESULTS = [MockSearchResult('core', 'MockModel', i, 1 - (i / 100.0)) for i in range(1, 100)]
+MOCK_SEARCH_RESULTS = [MockSearchResult('test_app', 'MockModel', i, 1 - (i / 100.0)) for i in range(1, 100)]
 MOCK_INDEX_DATA = {}
