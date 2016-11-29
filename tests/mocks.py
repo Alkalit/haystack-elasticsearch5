058bc41c7a66e3b529c90a6204212ccf8b44f065
Alexey Kalinin <lyoffa412@gmail.com>
2016-11-29 15:20:05 +0300

    preper futherer test cases

---
+++ tests/mocks.py
@@ -0,0 +1,13 @@
+
+from django.apps import apps
+
+from haystack.models import SearchResult
+
+
+class MockSearchResult(SearchResult):
+    def __init__(self, app_label, model_name, pk, score, **kwargs):
+        super(MockSearchResult, self).__init__(app_label, model_name, pk, score, **kwargs)
+        self._model = apps.get_model('core', model_name)
+
+MOCK_SEARCH_RESULTS = [MockSearchResult('core', 'MockModel', i, 1 - (i / 100.0)) for i in range(1, 100)]
+MOCK_INDEX_DATA = {}
--- tests/utils.py
+++ tests/utils.py
@@ -3,15 +3,16 @@ import elasticsearch
 from haystack import connections
 from django.conf import settings
 
+
 def clear_elasticsearch_index():
     # Wipe it clean.
     raw_es = elasticsearch.Elasticsearch(settings.HAYSTACK_CONNECTIONS['default']['URL'])
 
-    try:
+    main_index_exists = raw_es.indices.exists(index=settings.HAYSTACK_CONNECTIONS['default']['INDEX_NAME'])
+
+    if main_index_exists:
         raw_es.indices.delete(index=settings.HAYSTACK_CONNECTIONS['default']['INDEX_NAME'])
         raw_es.indices.refresh()
-    except elasticsearch.TransportError:
-        pass
 
     # Since we've just completely deleted the index, we'll reset setup_complete so the next access will
     # correctly define the mappings:
