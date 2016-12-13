779f2cd0d647f5df6361ec8e781322f240e10e5e
Alexey Kalinin <lyoffa412@gmail.com>
2016-12-13 20:49:19 +0300

    initial readme

---
+++ README.rst
@@ -0,0 +1,11 @@
+# haystack-elasticsearch5
+
+## Elasticsearch 5 backend for haystack.
+
+Elasticsearch 5 backend for haystack.
+
+Currently tested for python 3.5 and Django 1.10
+
+### Run tests
+
+Use `tox` or  `django-admin test --settings=tests.settings`. In second case environment should have django and haystack installed.
--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name="django-haystack-elasticsearch5",
     description = "Elasticsearch 5 supporting backend for django-haystack",
-    # license="Apache License, Version 2.0",
-    # url = "https://github.com/elastic/elasticsearch-py",
     version="0.0.1",
     packages=find_packages(),
     author = "Alexey Kalinin",
