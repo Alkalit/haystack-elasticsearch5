54c3d2afe3fb13b364d5cf4409225be4e28d0d5a
Alexey Kalinin <lyoffa412@gmail.com>
2016-12-18 23:01:37 +0300

    update setup

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-haystack-elasticsearch5",
-    description = "Elasticsearch 5 supporting backend for django-haystack",
-    version="0.0.1",
+    description = "Elasticsearch 5 backend for django-haystack",
+    version="0.7",
     packages=find_packages(),
     author = "Alexey Kalinin",
+    author_email = 'lyoffa412@gmail.com',
+    url = 'https://github.com/Alkalit/haystack-elasticsearch5',
+    download_url = 'https://github.com/Alkalit/haystack-elasticsearch5/tarball/0.7',
+    keywords = ['elasticsearch', 'haystack', 'search'],
+    classifiers = [],
 )
